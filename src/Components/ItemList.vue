2c7d824e4bb01d01441dd5b01e14c67334fbc801
tonutartust <jasper.sten@outlook.com>
2026-01-28 14:08:36 +0200

    Add modal

---
+++ src/Components/ItemList.vue
@@ -0,0 +1,47 @@
+<script setup>
+import { computed, ref } from 'vue';
+import ItemList from './Components/ItemList.vue';
+let i = 1;
+let items = ref([
+  {id: i++, text: 'sai', isDone: false},
+  {id: i++, text: 'piim', isDone: false},
+  {id: i++, text: 'viin',  isDone: false},
+  {id: i++, text: 'olu', isDone: false},
+  ]);
+let newItem = ref('');
+
+let doneItems = computed(() => items.value.filter(i => i.isDone));
+let toDoItems = computed(() => items.value.filter(i => !i.isDone));
+
+function add(){
+  if (newItem.value.trim() !== '') {
+     items.value.push({id: i++, text: newItem.value.trim(), isDone: false});
+  }
+  newItem.value = '';
+}
+</script>
+
+
+<template>
+  <div class="container"> 
+      <div class="field has-addons">
+      <div class="control">
+        <input class="input" type="text" placeholder="Find a repository" v-model="newItem"  @keypress.enter="add">
+      </div> 
+      <div class="control">
+        <button class="button is-info" @click="add">
+          Add item
+        </button>
+      </div>
+    </div>
+    <div class="content">
+      <ItemList :items="items"  title="All items"></ItemList>
+      <ItemList :items="doneItems" title="Done items"></ItemList>
+      <ItemList :items="toDoItems" title="ToDo items"></ItemList>
+    
+    </div>
+  </div>
+  
+</template>
+
+<style></style>
---
+++ src/Components/Modal.vue
@@ -0,0 +1,17 @@
+<script setup>
+  defineProps(['active']);
+</script>
+
+<template>
+
+ <div class="modal" :class="{'is-active': active}">
+      <div class="modal-background" @click="$emit('close')"></div>
+        <div class="modal-content">
+          <p class="image is-4by3">
+            <img src="https://bulma.io/assets/images/placeholders/1280x960.png" alt="">
+          </p>
+      </div>
+      <button class="modal-close is-large" aria-label="close" @click="$emit('close')"></button>
+   </div>
+
+</template>
