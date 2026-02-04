722e2507ec82e6aa25af9cac5fa8e7ae15d06936
tonutartust <jasper.sten@outlook.com>
2026-02-04 13:35:30 +0200

    added tabs

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,29 @@
 <script setup>
 
 import {ref} from 'vue';
-import Modal from './Components/Modal.vue';
-import ImgModal from './Components/ImgModal.vue';
+import Tabs from './Components/Tabs.vue';
 
-let modal1Active = ref(false);
-let modal2Active = ref(false);
+let titles = ref ([
+  'Item 1', 
+  'Item 2',
+  'Item 3', 
+  'Item 4'
+]);
+let activeTab = ref (0);
+let cotents = ref ([
+  'Content 1',
+  'Content 2',
+  'Content 3',
+  'Content 4'
+]);
 
 </script>
+
 <template>
-
-  <div class="container">
-    <button class="button is-primary" @click="modal1Active=true">Open Modal</button>
-    <button class="button is-link" @click="modal2Active=true">Open Modal 2</button>
-
-    <Modal :active="modal1Active" @close="modal1Active=false">
-      <div class="box">im in a box</div>
-    </Modal>
-
-    <ImgModal :active="modal2Active" @close="modal2Active=false" url="https://picsum.photos/1280/960?r=2"></ImgModal>
-  </div>
+    <Tabs :titles="titles" :active="activeTab" @setActive="activeTab = $event"></Tabs>
+    <div class="container">
+      {{ cotents[activeTab] }}
+    </div>
 </template>
 
 <style></style>
---
+++ src/Components/Tabs.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+defineProps (['active', 'titles']);
+
+</script>
+
+<template>
+
+    <div class="tabs is-centered">
+      <ul>
+        <li v-for="(title, index) in titles" :class="{'is-active' : index === active}" @click="$emit('setActive', index)">
+          <a>{{ title }}</a></li>
+      </ul>
+    </div>
+
+</template>
