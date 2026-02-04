e18bca3007c9a0217c4841a5b27ba6b38dc042fb
tonutartust <jasper.sten@outlook.com>
2026-02-04 13:01:28 +0200

    added image

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,23 @@
 
 import {ref} from 'vue';
 import Modal from './Components/Modal.vue';
+import ImgModal from './Components/ImgModal.vue';
 
 let modal1Active = ref(false);
+let modal2Active = ref(false);
 
 </script>
 <template>
 
   <div class="container">
     <button class="button is-primary" @click="modal1Active=true">Open Modal</button>
+    <button class="button is-link" @click="modal2Active=true">Open Modal 2</button>
 
-    <Modal :active="modal1Active" @close="modal1Active=false"></Modal>
+    <Modal :active="modal1Active" @close="modal1Active=false">
+      <div class="box">im in a box</div>
+    </Modal>
+
+    <ImgModal :active="modal2Active" @close="modal2Active=false" url="https://picsum.photos/1280/960?r=2"></ImgModal>
   </div>
 </template>
 
--- src/Components/Modal.vue
+++ src/Components/Modal.vue
@@ -1,5 +1,5 @@
 <script setup>
-  defineProps(['active']);
+  defineProps(['active', 'url']);
 </script>
 
 <template>
@@ -7,9 +7,9 @@
  <div class="modal" :class="{'is-active': active}">
       <div class="modal-background" @click="$emit('close')"></div>
         <div class="modal-content">
-          <p class="image is-4by3">
-            <img src="https://bulma.io/assets/images/placeholders/1280x960.png" alt="">
-          </p>
+
+
+          <slot></slot>
       </div>
       <button class="modal-close is-large" aria-label="close" @click="$emit('close')"></button>
    </div>
