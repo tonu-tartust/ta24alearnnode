fc6eac68fdc8afddc4f1f1a857480cce18810635
tonutartust <jasper.sten@outlook.com>
2026-03-18 13:23:22 +0200

    add fetch callback

--- src/Pages/ChuckNorris.vue
+++ src/Pages/ChuckNorris.vue
@@ -4,16 +4,11 @@ import { ref } from 'vue';
 
 let joke = ref('');
 
-function reqListener() {
-  let data = JSON.parse(this.responseText);
-  console.log(data.value);
-  joke.value = data.value;
-}
-
-const req = new XMLHttpRequest();
-req.addEventListener("load", reqListener);
-req.open("GET", "https://api.chucknorris.io/jokes/random");
-req.send();
+let req = fetch('https://api.chucknorris.io/jokes/random').then(res => {  
+    return res.json();
+}).then(data => {
+    joke.value = data.value;
+});
 
 </script>
 
