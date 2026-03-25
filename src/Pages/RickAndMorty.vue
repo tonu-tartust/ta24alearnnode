93b9eb5587a41e33352d0ddf1f36f1afc31aa0ab
tonutartust <jasper.sten@outlook.com>
2026-03-25 14:11:48 +0200

    Prev nad Next

--- src/Pages/RickAndMorty.vue
+++ src/Pages/RickAndMorty.vue
@@ -2,64 +2,43 @@
 
   import axios from 'axios';
   import { ref } from 'vue';
+  import CharacterCard from '../Components/CharacterCard.vue';
 
   let characters = ref([]);
 
-  let res = await axios.get('https://rickandmortyapi.com/api/character');
-  characters.value = res.data.results;
+  let pagination  = ref({
+    count: 0,
+    pages: 0,
+    next: null,
+    prev: null
+  });
+
+  await getCharacters('https://rickandmortyapi.com/api/character');
+
+  async function getCharacters(url) {
+      let res = await axios.get(url);
+      characters.value = res.data.results;
+      pagination.value = res.data.info;
+
+  }
+
+  async function next() {
+    await getCharacters(pagination.value.next);
+  }
+
+  async function prev() {
+    await getCharacters(pagination.value.prev);
+  }
 
 </script>
 <template>
 
   <div class="container">
+    <button class="button is-primary" :disabled="!pagination.prev" @click="prev">Prev</button>
+    <button class="button is-primary" :disabled="!pagination.next" @click="next">Next</button>
     <div class="columns is-multiline">
       <div class="column is-3" v-for="character in characters">
-        <div class="card">
-          <header class="card-header">
-            <p class="card-header-title">{{ character.name }}</p>
-          </header>
-          <div class="card-image">
-            <figure class="image is-4by3">
-              <img :src="character.image" />
-            </figure>
-          </div>
-          <div class="card-content">
-            <div class="content">
-              <table>
-                <tbody>
-                  <tr>
-                    <th>Status</th>
-                    <td>{{ character.status }}</td>
-                  </tr>
-                  <tr>
-                    <th>Gender</th>
-                    <td>{{ character.gender }}</td>
-                  </tr>
-                  <tr>
-                    <th>Type</th>
-                    <td>{{ character.type ? character.type : 'No type' }}</td>
-                  </tr>
-                  <tr>
-                    <th>Species</th>
-                    <td>{{ character.species }}</td>
-                  </tr>
-                  <tr>
-                    <th>Location</th>
-                    <td>{{ character.location.name }}</td>
-                  </tr>
-                  <tr>
-                    <th>Origin</th>
-                    <td>{{ character.origin.name }}</td>
-                  </tr>
-                  <tr>
-                    <th>Episodes</th>
-                    <td>{{ character.episode.length }}</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-          </div>
-        </div>
+        <CharacterCard :character="character"></CharacterCard>
       </div>
     </div>
   </div>
